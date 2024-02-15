f6826c1db1559329ae7363b89da5581a8bbcbdb4
devlop3s <itamaralopes41@gmail.com>
2024-02-15 01:22:11 -0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,116 @@
+//essa função é apenas o "gatilho" de todas as outras funções para criptografia da mensagem
+function botaoEncrypt() {
+  if (collectData() == "") {
+    alert("O campo de texto está vazio, insira alguma informação");
+    return;
+  } else {
+    hideDisplay();
+    collectData();
+    lowerCaseNormalize();
+    encryptedData();
+    alignData();
+    showData();
+    botaoCopy();
+  }
+}
+
+//função de "gatilho" de todas as outras fumções para criptografia da mensagem
+function botaoDecrypt() {
+  if (collectData() == "") {
+    alert("O campo de texto está vazio, insira alguma informação");
+    return;
+  } else {
+    hideDisplay();
+    collectData();
+    lowerCaseNormalize();
+    decryptedData();
+    alignData();
+    showDataDecrypted();
+    botaoCopy();
+  }
+}
+
+//essa função esconde a imagem do menino com a lupa e o texto de mensagem não encontrada que estão no retangulo lateral
+function hideDisplay() {
+  conjuntoRetangulo = document.getElementById(
+    "conjuntoDentroRetangulo"
+  ).style.display = "hidden";
+}
+
+//essa função coleta o texto inserido pelo usuário através da textarea <---- colocar um if else, se tiver uma palavra no campo que coleta a informação continuar o processo de
+//funções normalmente, senão interroper e mostrar um aviso de campo vazio
+function collectData() {
+  let dataInput = document.getElementById("dataInput").value;
+  return dataInput;
+}
+
+//função que transforma o texto coletado em minusculo além de substituir acentos para palavras sem acentos
+function lowerCaseNormalize() {
+  let normalData = collectData()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  return normalData.toLowerCase();
+}
+
+//função que criptografa(apenas troca as vogais por palavras) e retorna o texto "criptografado"
+function encryptedData() {
+  let trocarVogais = lowerCaseNormalize()
+    .replace("e", "enter")
+    .replace("i", "imes")
+    .replace("a", "ai")
+    .replace("o", "ober")
+    .replace("u", "ufat");
+  return trocarVogais;
+}
+
+//função que descriptografa(troca as palavras por vogais) e retorna o texto "descriptografado"
+function decryptedData() {
+  let trocarPalavrasPorVogais = lowerCaseNormalize()
+    .replace("enter", "e")
+    .replace("imes", "i")
+    .replace("ai", "a")
+    .replace("ober", "o")
+    .replace("ufat", "u");
+  return trocarPalavrasPorVogais;
+}
+
+//função que altera as classes do retangulo lateral para alinhar o texto ao inicio do retangulo
+function alignData() {
+  let RetanguloAlign = document.getElementById("conteudo-lateral");
+  RetanguloAlign.classList.remove("conteudo_lateral");
+  RetanguloAlign.classList.add("conteudo_lateral_jsalt");
+}
+
+//função que insere um texto via innerText ao retangulo
+function showData() {
+  let conjuntoRetangulo = document.getElementById("conjuntoDentroRetangulo");
+  conjuntoRetangulo.innerText = encryptedData();
+  conjuntoRetangulo.classList.remove("conteudo_lateral_original");
+  conjuntoRetangulo.classList.add("estiloResultado");
+  return conjuntoRetangulo;
+}
+
+function showDataDecrypted() {
+  let conjuntoRetangulo = document.getElementById("conjuntoDentroRetangulo");
+  conjuntoRetangulo.innerText = decryptedData();
+  conjuntoRetangulo.classList.remove("conteudo_lateral_original");
+  conjuntoRetangulo.classList.add("estiloResultado");
+}
+//função que cria o botão de copiar
+function botaoCopy() {
+  let botaoDeCopiar = document.createElement("botton");
+  let botaoTextoCopiar = document.createTextNode("Copiar");
+  botaoDeCopiar.setAttribute("id", "botaoCopy");
+  botaoDeCopiar.appendChild(botaoTextoCopiar);
+  botaoDeCopiar.classList.add("botao_estilo_copiar");
+  conjuntoRetangulo = document.getElementById("conjuntoDentroRetangulo");
+  conjuntoRetangulo.appendChild(botaoDeCopiar);
+  botaoDeCopiar.addEventListener("click", function () {
+    botaoDeCopiar.style.display = "none";
+    let textoCopiado = document.getElementById(
+      "conjuntoDentroRetangulo"
+    ).innerText;
+    navigator.clipboard.writeText(textoCopiado);
+    botaoDeCopiar.style.display = "flex";
+  });
+}
